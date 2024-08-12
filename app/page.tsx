23fcdb98bd9d0faf36dec2e99b729ee489809aa3
Jarren San Jose <sanjose.jarren@gmail.com>
2024-08-11 18:32:47 -0700

    trader meetups

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-2xl font-bold">
-        hi
+        trader meetups
       </h1>
     </main>
   );
